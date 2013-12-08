1f73d300ca2bad732562c7e5f83bf88641cd5804
liuji <liujiboy@163.com>
2013-12-08 23:50:09 +0800

    maze1 modified

--- section2.4/maze1/main.cpp
+++ section2.4/maze1/main.cpp
@@ -9,6 +9,7 @@ TASK:maze1
 #include <cstdio>
 #include <cmath>
 #include <sstream>
+#include <queue>
 using namespace std;
 ifstream fin("maze1.in");
 ofstream fout("maze1.out");
@@ -18,80 +19,71 @@ int h;
 typedef struct{
 	int x;
 	int y;
-}door;
-door exits[2];
+}pos;
+pos exits[2];
 int d1[2*38+1][2*100+1];
 int d2[2*38+1][2*100+1];
-void find(int distances[2*38+1][2*100+1],door exit)
+void find(int d[2*38+1][2*100+1],pos exit)
 {
-	/*for(int y=0;y<2*h+1;y++)
+	queue<pos> q;
+	d[exit.x][exit.y]=1;
+	q.push(exit);
+	while(!q.empty())
 	{
-		for(int x=0;x<2*w+1;x++)
+		pos p=q.front();
+		q.pop();
+		int x=p.x;
+		int y=p.y;
+		if(x-1>=0&&maze[x-1][y]==' '&&d[x-1][y]==0)
 		{
-			if(maze[x][y]==' ')
-			{
-				distances[x][y]=0;
-			}
-			else
-			{
-				distances[x][y]=-1;
-			}
-		}
-	}*/
-	distances[exit.x][exit.y]=1;
-	bool flag=true;
-	while(flag)
-	{
-		flag=false;
-		for(int y=0;y<2*h+1;y++)
-		{
-			for(int x=0;x<2*w+1;x++)
-			{
-
-					//cout<<x<<" "<<y<<endl;
-				if(distances[x][y]!=0)
-				{
-					//x-1,y
-					if(x-1>=0&&maze[x-1][y]==' '&&distances[x-1][y]==0)
-					{
-						distances[x-1][y]=distances[x][y]+1;
-						flag=true;
-					}
-					//x+1,y
-					if(x+1<2*w+1&&maze[x+1][y]==' '&&distances[x+1][y]==0)
-					{
-						distances[x+1][y]=distances[x][y]+1;
-						flag=true;
-					}
-					//x,y-1
-					if(y-1>=0&&maze[x][y-1]==' '&&distances[x][y-1]==0)
-					{
-						distances[x][y-1]=distances[x][y]+1;
-						flag=true;
-					}
-					//x,y+1
-					if(y+1<2*h+1&&maze[x][y+1]==' '&&distances[x][y+1]==0)
-					{
-						distances[x][y+1]=distances[x][y]+1;
-						flag=true;
-					}
-					/*for(int y=0;y<2*h+1;y++)
-					{
-						for(int x=0;x<2*w+1;x++)
-						{
-							cout<<distances[x][y]<<"\t";
-						}
-						cout<<endl;
-					}
-					cin.get();
-*/
-				}
-			}
+			d[x-1][y]=d[x][y]+1;
+			pos n;
+			n.x=x-1;
+			n.y=y;
+			q.push(n);
 		}
 
+		//x+1,y
+		if(x+1<2*w+1&&maze[x+1][y]==' '&&d[x+1][y]==0)
+		{
+			d[x+1][y]=d[x][y]+1;
+			pos n;
+			n.x=x+1;
+			n.y=y;
+			q.push(n);
+		}
+		//x,y-1
+		if(y-1>=0&&maze[x][y-1]==' '&&d[x][y-1]==0)
+		{
+			d[x][y-1]=d[x][y]+1;
+			pos n;
+			n.x=x;
+			n.y=y-1;
+			q.push(n);
+		}
+		//x,y+1
+		if(y+1<2*h+1&&maze[x][y+1]==' '&&d[x][y+1]==0)
+		{
+			d[x][y+1]=d[x][y]+1;
+			pos n;
+			n.x=x;
+			n.y=y+1;
+			q.push(n);
+		}
+		/*for(int y=0;y<2*h+1;y++)
+		  {
+		  for(int x=0;x<2*w+1;x++)
+		  {
+		  cout<<distances[x][y]<<"\t";
+		  }
+		  cout<<endl;
+		  }
+		  cin.get();
+		  */
 	}
-
 }
+
+
 int main() {
 	fin>>w>>h;
 	fin.get();
@@ -113,25 +105,25 @@ int main() {
 		fin.get(c);
 	}
 	/*for(int y=0;y<2*h+1;y++)
-	{
-		for(int x=0;x<2*w+1;x++)
-		{
-			cout<<maze[x][y];
-		}
-		cout<<endl;
-	}*/
+	  {
+	  for(int x=0;x<2*w+1;x++)
+	  {
+	  cout<<maze[x][y];
+	  }
+	  cout<<endl;
+	  }*/
 	//int d1[2*38+1][2*100+1];
 	//int d2[2*38+1][2*100+1];
 	find(d1,exits[0]);
 	/*for(int y=0;y<2*h+1;y++)
-	{
-		for(int x=0;x<2*w+1;x++)
-		{
-			cout<<d1[x][y]<<"\t";
-		}
-		cout<<endl;
-	}
-*/
+	  {
+	  for(int x=0;x<2*w+1;x++)
+	  {
+	  cout<<d1[x][y]<<"\t";
+	  }
+	  cout<<endl;
+	  }
+	 */
 	find(d2,exits[1]);
 	int max=0;
 	for(int y=0;y<2*h+1;y++)
