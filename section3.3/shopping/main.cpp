4d963e4bbce60953cd4d5165b82b3d0e752d5d05
liuji <liuji@liuji-desktop.(none)>
2014-01-19 23:12:49 +0800

    shopping solved

--- section3.3/shopping/main.cpp
+++ section3.3/shopping/main.cpp
@@ -28,48 +28,79 @@ struct offer{
 	int p;
 };
 vector<offer> offers;
-map<int,product> products;
-int min_p=0x7FFFFFFF;
-void cal(map<int,product> products,int total);
-bool sub(offer of,map<int,product> products,int total)
+product order[1000];
+vector<int> keys;
+int best[55556];
+int cal();
+int sub(offer& of)
 {
 	for(int i=0;i<of.products.size();i++)
 	{
 			product pr=of.products[i];
-			if(products[pr.c].k>=pr.k)
-			{
-				products[pr.c].k-=pr.k;	
-			}else
-			{
-				return false;
-			}
+			if(order[pr.c].k<pr.k)
+				return 0x7FFFFFFF;
 	}
-	cal(products,total+of.p);
-	return true;
+	for(int i=0;i<of.products.size();i++)
+	{
+			product pr=of.products[i];
+			order[pr.c].k-=pr.k;	
+	}
+
+	int total=cal()+of.p;
+	for(int i=0;i<of.products.size();i++)
+	{
+			product pr=of.products[i];
+			order[pr.c].k+=pr.k;	
+	}
+/*	for(map<int,product>::iterator it=products.begin();it!=products.end();it++)
+		{
+			product pr=it->second;
+			cout<<pr.c<<" "<<pr.k<<endl;
+		}
+	cout<<"total:"<<total<<endl;
+	cin.get();*/
+	return total;
 }
-void cal(map<int,product> products,int total)
+int getkey()
 {
-	bool flag=true;
+	int key=0;
+	for(int i=0;i<keys.size();i++)
+	{
+		int index=keys[i];
+		key=key*10+order[index].k;
+	}
+	return key;
+}
+int cal()
+{
+	/*string pid=tostring(products);
+	if(tmp[pid]!=0)
+		return tmp[pid];*/
+	int key=getkey();
+	if(best[key]!=0)
+		return best[key];
+	int min_p=0x7FFFFFFF;
 	for(int i=0;i<offers.size();i++)
 	{
 		offer of=offers[i];
-		if(sub(of,products,total))
-		{
-			flag=false;
-		}
-	}
-	if(flag)
-	{
-		for(map<int,product>::iterator it=products.begin();it!=products.end();it++)
-		{
-			product pr=it->second;
-			total+=pr.p*pr.k;
-		}
+		int total=sub(of);
 		if(total<min_p)
-		{
 			min_p=total;
-		}
 	}
+	if(min_p==0x7FFFFFFF)
+	{
+		int total=0;
+		for(int i=1;i<=999;i++)
+		{
+			product pr=order[i];
+			if(pr.k!=0)
+				total+=pr.p*pr.k;
+		}
+		min_p=total;
+	
+	}
+	best[key]=min_p;
+	return min_p;
 }
 int main() {
 	int n;
@@ -93,10 +124,10 @@ int main() {
 	{
 		product pr;
 		fin>>pr.c>>pr.k>>pr.p;
-		products[pr.c]=pr;
+		keys.push_back(pr.c);
+		order[pr.c]=pr;
 	}
-	cal(products,0);
-	fout<<min_p<<endl;
+	fout<<cal()<<endl;
 	fin.close();
 	fout.close();
 	return 0;
