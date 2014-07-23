76e3d9dc78c70b43f5183d0b84c1404178e049d6
liuji <liuji@163.com>
2014-07-23 17:04:41 +0800

    fence8 improved

--- section4.1/fence8/main.cpp
+++ section4.1/fence8/main.cpp
@@ -2,89 +2,90 @@
 LANG:C++
 TASK:fence8
 */
-#include<stdio.h>
-#include<stdlib.h>
-int v[55],wx[55],lwh[1111],ans=0,n,m,sumv=0,maxstep,space;
-int minlwh[1111],pp,max1[55],l,r,mid;
-int cmp(const void *a,const void *b)
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ifstream fin("fence8.in");
+ofstream fout("fence8.out");
+int boards[51];
+int rails[1024];
+int min_length[1024]; //min_length[i] represents the sum of the lenth of  1-ith rails
+int waste;
+int total_length; //total length of all boards
+int boards_len;
+int rails_len;
+int rail_num_by_cut=0;
+bool test(int index,int start)
 {
-	return *(int *)a-*(int *)b;
-}
-int cmp2(const void *a,const void *b)
-{
-	return -*(int *)a+*(int *)b;
-}
-int sou(int step,int pos)
-{
-
-	if(step<1)
-		return 1;
-	if(space>sumv-minlwh[mid])
-		return 0;
-
-	/*if(step>mid){
-		return 0;
-	}*/
-	int i,j,k,f[1111]={0};
-	for(i=n;i>=pos;i--)
+	if(index<1)
+		return true;
+	if(total_length-waste<min_length[rail_num_by_cut])
+		return false;
+	for(int i=boards_len;i>=start;i--)
 	{
-
-		if(v[i]>=lwh[step])
+		if(boards[i]>=rails[index])
 		{
-			//if(f[v[i]]==1)continue;//此优化巨牛X，可惜空间太。。开个f[11111]
-			//else f[v[i]]=1;
-			v[i]-=lwh[step];
-			if(v[i]<lwh[1])
-				space+=v[i];
-			int ok=0;
-			if(lwh[step]==lwh[step-1])
-			{
-				ok=sou(step-1,i);
-			}                               
+			boards[i]-=rails[index];
+			if(boards[i]<rails[1])
+				waste+=boards[i];
+			
+			bool reval;
+			if(rails[index-1]==rails[index])
+				reval=test(index-1,i);
 			else
-			{
-			      	ok=sou(step-1,1);
-			}
-			if(v[i]<lwh[1])
-				space-=v[i];
-			v[i]+=lwh[step];
-			if(ok==1)
-				return 1;
+				reval=test(index-1,1);
+			if(boards[i]<rails[1])
+				waste-=boards[i];
+			boards[i]+=rails[index];
+			if(reval==true)
+				return true;
 		}
 	}
-	return 0;
+	return false;
 }
-int main()
-{
-	freopen("fence8.in","r",stdin);
-	freopen("fence8.out","w",stdout);
-	int i,j,k;
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
+int main() {
+	fin>>boards_len;
+	for(int i=1;i<=boards_len;i++)
 	{
-		scanf("%d",&v[i]); 
-		sumv+=v[i];
+		fin>>boards[i];
+		total_length+=boards[i];
 	}
-	scanf("%d",&m);
-	for(i=1;i<=m;i++)
-		scanf("%d",&lwh[i]);
-	//while(sumv<minlwh[m])m--;
-	qsort(&v[1],n,sizeof(v[1]),cmp);
-	qsort(&lwh[1],m,sizeof(lwh[1]),cmp);
-	for(i=1;i<=m;i++)
-		minlwh[i]=minlwh[i-1]+lwh[i];
-	l=0;r=m;mid=(l+r)/2;
-	while(l<=r)
+	fin>>rails_len;
+	for(int i=1;i<=rails_len;i++)
 	{
-		space=0;
-		//for(i=1;i<=n;i++)
-			//v[i]=v[i]; 
-		if(sou(mid,1)==1)
-			l=mid+1;
-		else 
-			r=mid-1;
-		mid=(l+r)/2;
+		fin>>rails[i];
+		min_length[i]=rails[i]+min_length[i-1];
 	}
-	printf("%d\n",mid);
+	sort(rails,rails+rails_len+1);
+	sort(boards,boards+boards_len+1);
+	for(int i=1;i<=rails_len;i++)
+	{
+		if(rails[i]<=boards[boards_len])
+		{
+			min_length[i]=rails[i]+min_length[i-1];
+		}
+		else
+		{
+			rails_len=i;
+			break;
+		}
+	}
+	int high=rails_len;
+	int low=0;
+	rail_num_by_cut=(high+low)/2;
+	while(low<=high)
+	{
+		waste=0;
+		if(test(rail_num_by_cut,1))
+			low=rail_num_by_cut+1;
+		else
+			high=rail_num_by_cut-1;
+
+		rail_num_by_cut=(high+low)/2;
+	}
+	fout<<rail_num_by_cut<<endl;
+	fin.close();
+	fout.close();
 	return 0;
 }
