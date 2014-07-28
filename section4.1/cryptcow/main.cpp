123d5d87b46a9cdb6f0dfe86ba6d9e215d6c9fed
liuji <liujiboy@163.com>
2014-07-29 01:08:27 +0800

    cryptcow solved

--- section4.1/cryptcow/main.cpp
+++ section4.1/cryptcow/main.cpp
@@ -13,7 +13,6 @@ using namespace std;
 ifstream fin("cryptcow.in");
 ofstream fout("cryptcow.out");
 string target="Begin the Escape execution at the Break of Dawn";
-map<char,int> m;
 struct Position{
 	int pos[9];
 	int len;
@@ -44,55 +43,146 @@ struct SubString{
 		sub[len++]=s;
 	}
 };
+map<long,int> m;
+inline long elf(const char *str) {
+
+	int i=0;
+	long g,sum=0;
+	while (*str)
+	{
+		sum=(sum<<4)+((*str++)-97+1);//将hash值左移4位后加上字符ascii
+		g=sum & 0xf0000000;//取出hash值的高4位
+		if (g) sum^=g>>24;//因为下一步仍要左移4位，所以如果高4位不为0，进行下面处理，防止丢失信息 将高4位与5~8位xor
+		sum&=~g;//消除下次可能被移到符号位的4个位
+		i++;
+	}
+	return sum%99991;
+}
+inline int search(string&word)
+{
+	//const char *str=word.c_str();
+	//long hash=elf(str);
+	//if(m[hash]!=0)
+	//	return m[hash];
+	if(target.find(word)==string::npos)
+	{
+	//	m[hash]=-1;
+		return -1;
+	}
+	else
+	{
+	//	m[hash]=1;
+		return 1;
+	}
+}
 
 inline int find(string&input)
 {
+	if(m.find(elf(input.c_str()))!=m.end())
+	{
+	//	cout<<m.find(elf(input.c_str()))->first<<endl;
+		return 0;
+	}
 	Position cpos,opos,wpos;
 	//SubString words;
-	string word="";
+	string word="",co="",ow="";
+	string state="S";
 	for(int i=0;i<input.size();i++)
 	{
 		char c=input.at(i);
+		//cout<<state<<"\t"<<c<<endl;
 		switch(c){
 			case 'C':
 				cpos.push_back(i);
-				if(word!="")
+				if(state=="S")
 				{
-					//words.push_back(word);
-					if(target.find(word)==string::npos)
-						return 0;
-					word="";
+					state="C";
+					if(word!="")
+					{
+						if(target.find(word)!=0)
+							return 0;
+						word="";
+					}
+				}else if(state=="C"||state=="CO"||state=="O"||state=="W")
+				{
+					state="C";
+					if(word!="")
+					{
+						if(search(word)==-1)
+						{
+							return 0;
+						}
+						word="";
+					}
+
 				}
 				break;
 			case 'O':
 				opos.push_back(i);
-				if(word!="")
+				if(state=="S")
 				{
-					if(target.find(word)==string::npos)
-						return 0;
-					word="";
-				}
+					return 0;
+				}else if(state=="C")
+				{
+					state="CO";
+					co=word;
+					if(word!="")
+					{
+						if(search(word)==-1)
+							return 0;
+						word="";
+					}
 
+				}else if(state=="CO"||state=="O"||state=="W")
+				{
+					state="O";
+					if(word!="")
+					{
+
+						if(search(word)==-1)
+							return 0;
+						word="";
+					}
+				}
 				break;
 			case 'W':
 				wpos.push_back(i);
-				if(word!="")
+				if(state=="S")
 				{
-					if(target.find(word)==string::npos)
+					return 0;
+				}else if(state=="C"||state=="O"||state=="W")
+				{
+					state="W";
+					if(word!="")
+					{
+
+						if(search(word)==-1)
+							return 0;
+						word="";
+					}
+
+
+				}else if(state=="CO")
+				{
+					state="W";
+					ow=word;
+					if(target.find(ow+co)==string::npos)
 						return 0;
 					word="";
 				}
-
 				break;
 			default:
 				word+=c;
 		}
 	}
-	if(word!="")
+	if(state!="W"&&state!="S")
 	{
-		if(target.find(word)==string::npos)
-			return 0;
+		return 0;
 
+	}
+	else if(state=="W"&&input.find_last_of(word)!=input.size()-1)
+	{
+		return 0;
 	}
 
 	if(cpos.len!=wpos.len&&wpos.len!=opos.len)
@@ -109,33 +199,10 @@ inline int find(string&input)
 			return 0;
 		}
 	}
-	/*for(int i=0;i<words.len;i++)
-	{
-		if(target.find(words.sub[i])==string::npos)
-			return 0;
-	}*/
-	if(!(cpos.front()<opos.front()&&opos.back()<wpos.back()))
+	/*if(!(cpos.front()<opos.front()&&opos.back()<wpos.back()))
 	{
 		return 0;
-	}
-	for(int i=0;i<cpos.pos[0];i++)
-	{
-		if(input.at(i)!=target.at(i))
-		{
-			return 0;
-		}
-	}
-
-	for(int i=0;i<input.size()-wpos.back()-1;i++)
-	{
-		//	cout<<i<<"\t"<<target.at(target.size()-i-1)<<"\t"<<input.at(input.size()-i-1)<<endl;
-		if(target.at(target.size()-i-1)!=input.at(input.size()-i-1))
-		{
-
-			return 0;
-
-		}
-	}
+	}*/
 	for(int i=0;i<cpos.len;i++)
 	{
 		int c=cpos.pos[i];
@@ -155,6 +222,8 @@ inline int find(string&input)
 					int retval=find(newInput);
 					if(retval)
 						return retval+1;
+					else
+						m[elf(newInput.c_str())]=0;
 				}
 			}
 
